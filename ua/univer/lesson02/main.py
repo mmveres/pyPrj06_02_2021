161ecee9929c3d9398dd6e8800002c64a200b8e9
mmveres <mmveres@gmail.com>
2021-02-13 11:51:14 +0200

    lesson02 def func

--- ua/univer/lesson02/main.py
+++ ua/univer/lesson02/main.py
@@ -7,13 +7,20 @@ def find_min(x,y):
     else:
         return y
 
+def print_min(x,y):
+    if x < y:
+        mymin = x
+    else:
+        mymin = y
+    print("mymin =", mymin)
+
 a = 2
 b = 1
 c = 3
 d = 3
 
-min_local1 = find_min(a,b)
-min_local2 = find_min(c,d)
-min_local = find_min(min_local1,min_local2)
+print_min(10,20)
+
+min_local = find_min(find_min(a,b),find_min(c,d))
 
 print(min_local)
