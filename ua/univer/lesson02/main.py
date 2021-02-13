cd92c46aac004f9ecb77d2a89c262d8af9c9378d
mmveres <mmveres@gmail.com>
2021-02-13 11:35:22 +0200

    lesson02 task1 find min 4 values

--- ua/univer/lesson02/main.py
+++ ua/univer/lesson02/main.py
@@ -1,1 +1,19 @@
-print("hi")
+# 1. Даны 4 числа типа int. Сравнить их и вывести наименьшее на консоль.
+# 2. Вывести на консоль количество максимальных чисел среди этих четырех.
+
+def find_min(x,y):
+    if x < y:
+        return x
+    else:
+        return y
+
+a = 2
+b = 1
+c = 3
+d = 3
+
+min_local1 = find_min(a,b)
+min_local2 = find_min(c,d)
+min_local = find_min(min_local1,min_local2)
+
+print(min_local)
