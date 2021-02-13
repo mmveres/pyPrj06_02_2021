5d73bfc3af976c87cde9cd52a21ac537c9bcc4c9
mmveres <mmveres@gmail.com>
2021-02-13 12:02:51 +0200

    lesson02 task2 find max count

--- ua/univer/lesson02/main.py
+++ ua/univer/lesson02/main.py
@@ -7,6 +7,12 @@ def find_min(x,y):
     else:
         return y
 
+def find_max(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+
 def print_min(x,y):
     if x < y:
         mymin = x
@@ -23,4 +29,19 @@ print_min(10,20)
 
 min_local = find_min(find_min(a,b),find_min(c,d))
 
-print(min_local)
+print("min =",min_local)
+
+
+max_local = find_max(find_max(a,b),find_max(c,d))
+
+count_max = 0
+if max_local == a :
+    count_max+=1
+if max_local == b :
+    count_max+=1
+if max_local == c :
+    count_max+=1
+if max_local == d :
+    count_max+=1
+print("max =",max_local)
+print("count max =",count_max)
